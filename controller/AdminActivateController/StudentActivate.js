504f0725ec08b8d040ca272aded30eb4bc7c445c
Busayo gabriel <omotukabusayo22@gmail.com>
2023-12-12 05:51:12 +0100

    student activation update two

--- controller/AdminActivateController/StudentActivate.js
+++ controller/AdminActivateController/StudentActivate.js
@@ -1,20 +1,25 @@
 const addStudent = require("../../Model/addStudent");
 
-const activateStudent = (req, res) => {
+const activateStudent = async(req, res) => {
     const { studentId } = req.params;
   
-    addStudent.findByIdAndUpdate(
-      studentId,
-      { isActive: true },
-      { new: true },
-      (err, activatedStudent) => {
-        if (err || !activatedStudent) {
-          return res.status(500).json({ error: 'Unable to activate student account.' });
-        }
-  
-        res.json({ message: 'Student account activated successfully.' });
+    try {
+      const updatedStudent = await addStudent.findByIdAndUpdate(
+        studentId,
+        {isActive: true},
+        {new: true}
+      )
+
+      if (!updatedStudent) {
+        return res.status(404).json({ error: 'Student not found' });
       }
-    );
+
+      res.json({message: 'Student account activated successfully.'})
+    } catch (error) {
+      console.error('Error activating student:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+
   };
 
 
