d54da2f3e3b0500c5b6876f9c446ffd974b6ca85
Busayo gabriel <omotukabusayo22@gmail.com>
2023-12-14 03:43:48 +0100

    updates

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const app = express()
 
 app.use(express.json());
 app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: false}));
 app.use(cors());
 
 //middleware
