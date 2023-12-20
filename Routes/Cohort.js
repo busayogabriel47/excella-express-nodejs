9548cc14a7cbc97b5ddb5551dff23b5afc1860eb
Busayo gabriel <omotukabusayo22@gmail.com>
2023-12-20 11:55:04 +0100

    student-cohort

--- Routes/Cohort.js
+++ Routes/Cohort.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const fetchAllCohorts = require('../controller/fetchAllCohort')
-
+const studentCohort = require('../controller/StudentCohort')
+const studentAuth = require("../middleware/requireLogin")
 
 
 
 router.get('/cohorts', fetchAllCohorts);
+router.get("/student/:studentId", studentCohort);
 
 
 module.exports = router
