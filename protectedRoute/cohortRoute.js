e9224b37176915c1a3e469ad6a7ae031c93182bb
Busayo gabriel <omotukabusayo22@gmail.com>
2023-12-22 16:21:26 +0100

    import path

--- protectedRoute/cohortRoute.js
+++ protectedRoute/cohortRoute.js
@@ -4,6 +4,8 @@ const Cohort = require('../Model/cohort')
 const adminAuth = require('../middleware/adminLogin')
 const {createCohort, updateCohort, deleteCohort} = require('../controller/CohortController')
 const multerMiddleware = require('../middleware/cohortBanner')
+const path = require("path")
+
 
 //Endpoint to add cohorts (accessible to admin only)
 router.post('/add-cohort', express.static(path.join(__dirname, 'uploads')), adminAuth, multerMiddleware, createCohort)
