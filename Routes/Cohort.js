0f81bff05600fce1a5f96cb1c85e70f94c123c50
Busayo gabriel <omotukabusayo22@gmail.com>
2024-01-12 23:53:19 +0100

    upoad assessment

--- Routes/Cohort.js
+++ Routes/Cohort.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const fetchAllCohorts = require('../controller/fetchAllCohort')
 const studentCohort = require('../controller/StudentCohort')
 const studentAuth = require("../middleware/requireLogin")
-
+const adminAuth = require("../middleware/adminLogin")
+const addFormUrlToCohort = require('../controller/uploadFormUrl')
 
 
 router.get('/cohorts', fetchAllCohorts);
 router.get("/student/:studentId", studentAuth, studentCohort);
-
+router.post('/cohorts/:cohortId/add-form-url', adminAuth, addFormUrlToCohort)
 
 module.exports = router
